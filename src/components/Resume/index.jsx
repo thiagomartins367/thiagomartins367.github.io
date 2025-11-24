4370896761181546e4fd3d48037284c74672c52f
THIAGO MARTINS <thiago17thiago@gmail.com>
2025-11-23 22:18:51 -0300

    refactor(index.jsx): :recycle: Componente "Resume"
    Seção "Experiência" movida para o topo na seção "section-resume"

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -8,45 +8,6 @@ function Resume() {
     <section className="section-resume">
       <section className="timeline">
         <div className="div-title-img-timeline">
-          <div className="div-img-icon-education">
-            <img src="/assets/stacked-book.svg" alt="stacked-book.svg" />
-          </div>
-          <h3 className="h3-title-timeline">
-            Educação
-          </h3>
-        </div>
-        <ul className="ul-list-timeline">
-          <li className="li-timeline-bottom-line-icon"><div /></li>
-          {
-            professional.education.map((itemObj) => (
-              <li className="li-timeline-item" key={ itemObj.title }>
-                <div className="div-marker-separator">
-                  <div className="div-timeline-item-marker">
-                    <div />
-                  </div>
-                </div>
-                <div className="div-timeline-item-content">
-                  <h4>{ itemObj.title }</h4>
-                  <span className="item-timeline-period">
-                    { `${itemObj.start} — ${itemObj.end}` }
-                  </span>
-                  {
-                    itemObj.description.map((paragraph, index) => (
-                      <p
-                        key={ `education-description-paragraph-${index}` }
-                        dangerouslySetInnerHTML={ { __html: paragraph } }
-                      />
-                    ))
-                  }
-                </div>
-              </li>
-            ))
-          }
-        </ul>
-      </section>
-
-      <section className="timeline">
-        <div className="div-title-img-timeline">
           <div className="div-img-icon-experience">
             <img src="/assets/user-settings.svg" alt="user-settings.svg" />
           </div>
@@ -73,6 +34,45 @@ function Resume() {
                     itemObj.description.map((paragraph, index) => (
                       <p
                         key={ `experience-description-paragraph-${index}` }
+                        dangerouslySetInnerHTML={ { __html: paragraph } }
+                      />
+                    ))
+                  }
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+
+      <section className="timeline">
+        <div className="div-title-img-timeline">
+          <div className="div-img-icon-education">
+            <img src="/assets/stacked-book.svg" alt="stacked-book.svg" />
+          </div>
+          <h3 className="h3-title-timeline">
+            Educação
+          </h3>
+        </div>
+        <ul className="ul-list-timeline">
+          <li className="li-timeline-bottom-line-icon"><div /></li>
+          {
+            professional.education.map((itemObj) => (
+              <li className="li-timeline-item" key={ itemObj.title }>
+                <div className="div-marker-separator">
+                  <div className="div-timeline-item-marker">
+                    <div />
+                  </div>
+                </div>
+                <div className="div-timeline-item-content">
+                  <h4>{ itemObj.title }</h4>
+                  <span className="item-timeline-period">
+                    { `${itemObj.start} — ${itemObj.end}` }
+                  </span>
+                  {
+                    itemObj.description.map((paragraph, index) => (
+                      <p
+                        key={ `education-description-paragraph-${index}` }
                         dangerouslySetInnerHTML={ { __html: paragraph } }
                       />
                     ))
